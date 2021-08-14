fa2eac8836dec08653999e81f7fe5c151e72d026
sugus.grassi <sugus.grassi@gmail.com>
2021-08-14 17:37:06 -0300

    make it better

--- src/db.js
+++ src/db.js
@@ -20,7 +20,7 @@ const pool = new Pool({
 const sequelize = new Sequelize(
   process.env.NODE_ENV === "production" ? proConfig : devConfig, {
   logging: false, // set to console.log to see the raw SQL queries
-  native: true, // lets Sequelize know we can use pg-native for ~30% more speed
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   ssl: true, 
   dialectOptions: {
     ssl: true
